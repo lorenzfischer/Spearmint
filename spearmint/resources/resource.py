2e9b9a3eab92a29b69c28154b846e5529e66475f
Lorenz Fischer <lorenz.fischer@gmail.com>
2015-01-12 16:49:25 +0100

    now we can set max_finished_jobs to 0 on the resource to basically allow unlimited jobs

--- spearmint/resources/resource.py
+++ spearmint/resources/resource.py
@@ -302,7 +302,7 @@ class Resource(object):
         if self.numPending(jobs) >= self.max_concurrent:
             return False
         
-        if self.numComplete(jobs) >= self.max_finished_jobs:
+        if self.max_finished_jobs > 0 and self.numComplete(jobs) >= self.max_finished_jobs:
             return False
 
         return True 
